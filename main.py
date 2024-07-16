588dee0eabdb98ddaa97986691bdda6a6b9a9047
FromHeroToZero <p.vranka.pe@gmail.com>
2024-07-16 22:26:09 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from pet import Pet
+
+
+if __name__ == '__main__':
+    # Prompt the user to name their pet and load existing data if available
+    # Use name to construct Pet object
+    pet = Pet("Aira")
+
+    # Loop for options
+    # Handle user choice to feed the pet
+    # Handle user choice to play with the pet
+    # Handle user choice to groom the pet
+    # Handle user choice to save the game and exit
+    # Handle invalid user choice
+
+    # Introduce a random event affecting the pet's status
